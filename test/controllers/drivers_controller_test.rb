43fc5786d8dcfadddb151a3d3c14ca7bc32c75bc
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-02 16:53:46 -0700

    Created a controller for each model

---
+++ test/controllers/drivers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe DriversController do
+  # it "must be a real test" do
+  #   flunk "Need real tests"
+  # end
+end
---
+++ test/controllers/passengers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+describe PassengersController do
+  # it "must be a real test" do
+  #   flunk "Need real tests"
+  # end
+end
