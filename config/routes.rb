0f763c16f03c85e0513502d6f7f32ec1e016646c
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-02 16:57:45 -0700

    added all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+
+resources :drivers
+resources :trips
+resources :passengers
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
