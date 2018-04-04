1e41bb9b29945de42bff711e49142f92d28ae3cd
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-03 22:00:25 -0700

    Added business logic to driver model, with a average_rating and earnings methods. Then I added them to the driver/show

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,22 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def earnings
+    earnings = 0
+      self.trips.each do |trip|
+        earnings += trip.cost
+      end
+    return earnings
+  end
+
+  def average_rating
+    count = 0
+    stars = 0
+    self.trips.each do |trip|
+      count +=1
+      stars += trip.rating
+    end
+    return stars/count
+  end
+
 end
